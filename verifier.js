1b1184984fd4faec20cbcea042b78037c4a166c3
Andrei Onea <andreionea@gmail.com>
2012-09-13 06:58:40 -0700

    Added table verifier and corrected previous code

--- verifier.js
+++ verifier.js
@@ -2,9 +2,9 @@ var verifier = {}
 verifier.isValidUsername = function(username) {
     if (typeof(username) != "string")
         return {"ok":false, "what":"Username must be string"};
-    if (username.length < 4 || username.length > 15)
+    if (username.length < 5 || username.length > 15)
         return {"ok":false, "what":"Username must have between 5 and 15 characters"};
-    var regexRule = new RegExp("\w|-|_)*");    
+    var regexRule = new RegExp("(\w|-|_)*");    
     return {"ok":regexRule.test(username)};
 }
 verifier.isValidPassword = function(password) {
@@ -12,7 +12,33 @@ verifier.isValidPassword = function(password) {
         return{"ok":false, "what":"Password must be string"};
     if (password.length < 5 || password.length > 20)
         return {"ok":false, "what":"Password must be between 6 and 20 characters"};
-    var regexRule = new RegexRule("([\S]*[0-9]*)*");
-    return {"ok":regexRule.test(password)};
+    var regexRule = new RegExp("([\S]*[0-9]*)*");
+    if (regexRule,test(password))
+        return {"ok":true};
+    return {"ok":false, "what":"Password contains invalid characters"}
+}
+
+verifier.isArray = function( obj ) {
+    return toString.call(obj) === "[object Array]";
+};
+
+verifier.isValidTable = function(rows, cols, airplanes, table) {
+    if (toString.call(table) !== "[object Array]")
+        return {"ok":false, "what":"Table is not an array"};
+    if (table.length != rows)
+        return {"ok":false, "what":"Table has invalid number of lines"};
+    for (var i = 0; i < rows; ++i) {
+        if (toString.call(table[i]) !== "[object Array]")
+            return {"ok":false, "what":"Table row is not an array"};
+        if (table[i].length != cols)
+            return {"ok":false, "what":"Table row has invalid number of columns"};
+    }
+
+    for (var i = 0; i < rows; ++i)
+        for (var j = 0; j < rows; ++j) 
+            if (table[i][j] < 0 || table[i][j] > 5)
+                return {"ok":false, "what":"Table has invalid cell value"};
+    // TODO: Check if planes are actually well arranged
+    return {"ok": true};   
 }
 
