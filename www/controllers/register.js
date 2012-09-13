22e45beb3c3cc2ab99c489c219bc9619c60391df
Andrei Poenaru <andrei@kamino.(none)>
2012-09-13 15:06:12 +0300

    Login + Register fix

--- www/controllers/register.js
+++ www/controllers/register.js
@@ -2,11 +2,7 @@ var http = require('../../dbf/util.js');
 
 function solve(username, name, password) {
 	add.user(username, name, password, function(message) {
-		if (!message.ok) {
-			socket.emit("Error: " + message.what)
-		} else {
-			socket.emit("Register succesful")
-		}
+		socket.emit(message);
 	});
 }
 
