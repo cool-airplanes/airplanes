098fbe7e01431ad599bb060020f2f39ce3136634
Andrei Onea <andreionea@gmail.com>
2012-09-13 09:03:37 -0700

    Added notification

--- page/static/main.js
+++ page/static/main.js
@@ -1,6 +1,13 @@
 var socket = io.connect('http://127.0.0.1:8080');
 socket.on('register-response', function(message) {
-    alert(message.what);
+    //alert(message.what);    
+    var x = "<div class='alert";
+    if (message.ok)
+    	x += " alert-success'";
+    else
+    	x += " alert-error'";
+    x += ">" + message.what + " <button type='button' class='close'>x</button></div>";
+    $("#alert-wrapper").html(x);
 });
 
 $(function() {
@@ -13,5 +20,8 @@ $(function() {
 
         socket.emit('register', data.username, data.name, data.password);
     	return false;
+    });
+    $(document).on("click", ".alert > .close ", function() {
+    	$(this).parent().hide("slow");
     });
 });
