bdcc05bfff987857737c8bbd5e6285f62514d72e
Andrei Onea <andreionea@gmail.com>
2012-09-13 05:29:17 -0700

    Made verifier.js return dictionaries

--- verifier.js
+++ verifier.js
@@ -1,18 +1,18 @@
 var verifier = {}
 verifier.isValidUsername = function(username) {
-	if (typeof(username) != "string")
-        return false;
-    if (username.length < 4 || username.length > 14)
-        return false;
+    if (typeof(username) != "string")
+        return {"ok":false, "what":"Username must be string"};
+    if (username.length < 4 || username.length > 15)
+        return {"ok":false, "what":"Username must have between 5 and 15 characters"};
     var regexRule = new RegExp("\w|-|_)*");    
-    return regexRule.test(username);
+    return {"ok":regexRule.test(username)};
 }
 verifier.isValidPassword = function(password) {
     if (typeof(password) != "string")
-        return false;
-    if (password.length < 6 || password.length > 16)
-        return false;
+        return{"ok":false, "what":"Password must be string"};
+    if (password.length < 5 || password.length > 20)
+        return {"ok":false, "what":"Password must be between 6 and 20 characters"};
     var regexRule = new RegexRule("([\S]*[0-9]*)*");
-    return regexRule.test(password);  
+    return {"ok":regexRule.test(password)};
 }
 
