c30b436c4f262b32c1e9d314a65b237afef3203e
Andrei Poenaru <poenaru@adobe.com>
2012-09-18 17:36:41 +0300

    Implemented challenges. NOT tested yet

--- www/controllers/user.js
+++ www/controllers/user.js
@@ -40,7 +40,9 @@ function login(socket, username, password) {
             "password" : security.encrypt(password),
             "name" : result.name,
             "game" : undefined,
-            "socket" : socket
+            "socket" : socket,
+            "challenged" : {},
+            "challengedBy" : {}
         });
 
         session.lobby.set(username, true);
@@ -56,11 +58,15 @@ function logout(socket) {
         return;
     }
 
-    var username = session.sockets.get(socket.id).username;
-    session.users.remove(username);
-    session.lobby.remove(username);
+    var user = session.users.get(session.sockets.get(socket.id).username);
+
+    for (var )
+
+    session.users.remove(user.username);
+    session.lobby.remove(user.username);
     session.sockets.set(socket.id, { "socket" : socket});
-    sendToLobby('user-logout', {"username" : username});
+
+    sendToLobby('user-logout', {"username" : user.username});
 }
 
 function disconnect(socket) {
@@ -89,10 +95,75 @@ function sendUserList(socket) {
     socket.emit('user-list-response', userList);
 }
 
+function challengeQuestion(socket, data) {
+    var challenger = session.users.get(socket.username);
+    var challenged = session.users.get(data.opponent);
+
+    if (!challenged)
+        return;
+
+    if (challenger.challenged[challenged.username]) {
+        socket.emit('challenge-question-response', { ok: false, what: "Already challenged " + challenged.name });
+        return;
+    }
+
+    if (challenged.challenged[challenger.username]) {
+        socket.emit('challenge-question-response', { ok: false, what: "You are already challenged by " + challenged.name });
+        return;
+    }
+
+    challenger.challenged[challenged.username] = true;
+    challenged.challengedBy[challenger.username] = true;
+
+    challenged.socket.emit('challenge-received', { opponent: challenger.username });
+    challenger.socket.emit('challenge-question-response', { ok: true, what: "Challenge sent to " + challenged.name + "!", opponent: challenged.username });
+}
+
+function challengeAnswer(socket, data) {
+    var challenger = session.users.get(data.username);
+    var challenged = session.users.get(socket.username);
+
+    if (!challenger)
+        return;
+
+    if (!challenger.challenged[challenged.username] || !challenged.challengedBy[challenger.username]) {
+        socket.emit('challenge-answer-reponse', { ok: false, what: "Invalid challenge" });
+        return;
+    }
+
+    delete challenger.challenged[challenged.username];
+    delete challenged.challengedBy[challenger.username];
+
+    if (data.answer) {
+        startGame(challenger, challenged);
+        return;
+    }
+
+    challenger.socket.emit('challenge-rejected', { opponent: challenged.username });
+}
+
+function startGame(user1, user2) {
+    cancelSentChallenges(user1);
+    cancelSentChallenges(user2);
+}
+
+// Unexported
 function sendToLobby(event, data) {
     for (var username in session.lobby.data) {
         session.users.get(username).socket.emit(event, data);
     }
+}
+
+// Unexported
+function cancelSentChallenges(user) {
+    for (var username in user.challenged) {
+        var challenged = session.users.get(username);
+
+        delete challenged.challengedBy[user.username];
+        challenged.socket.emit('challenged-canceled', { opponent: user.username });
+    }
+
+    user.challenged = {};
 }
 
 module.exports.connect = connect;
