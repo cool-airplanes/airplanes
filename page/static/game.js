873537ca528f503e45c7a7a157987d323a38832f
Andrei Onea <andreionea@gmail.com>
2012-09-27 19:41:47 +0300

    Rotate on right click works, and user can add only 2 airplanes. Right click still brings out context menu, though

--- page/static/game.js
+++ page/static/game.js
@@ -31,6 +31,8 @@ Plane = function(orientation, gameboard, position)
     this.orientation = orientation;
     this.gameboard = gameboard;
     this.position = position;
+    this.identifier = GameBoard.nextPlaneID + "";
+    ++GameBoard.nextPlaneID;
 }
 
 Plane.Orientation = {
@@ -60,11 +62,6 @@ Plane.HorizontalRight = {
 
 Plane.orientation = Plane.Orientation.VerticalUp;
 
-Plane.changeOrientation = function(orientation)
-{
-    Plane.orientation = orientation;
-}
-
 Plane.prototype = {
     setProprieties: function(top, left)
     {
@@ -79,37 +76,42 @@ Plane.prototype = {
             return Plane.Orientation.HorizontalRight;
         else if (orientation === Plane.Orientation.HorizontalRight)
             return Plane.Orientation.VerticalDown;
-        else if (orientation === Plane.VerticalDown)
+        else if (orientation === Plane.Orientation.VerticalDown)
             return Plane.Orientation.HorizontalLeft;
         else return Plane.Orientation.VerticalUp;
     },
-
-    placeAirplane: function(event)
+    placeAirplane: function()
     {
-        var position = this.gameboard.getPositionFromId(event.target.id);
+        var position = this.position;
         console.log(position + " " + GameBoard.Dimensions.cellHeight);
-        if (this.gameboard.validatePosition(position))
+        if (this.gameboard.validatePosition(this.orientation, position))
         {
-            var dx = Plane[Plane.orientation].DX;
-            var dy = Plane[Plane.orientation].DY;
-            console.log(Plane.orientation);
+            var dx = Plane[this.orientation].DX;
+            var dy = Plane[this.orientation].DY;
+            console.log(this.orientation);
             for (var i = 0; i < dx.length; ++i) {
                 var nrow = position[0] + dx[i];
                 var ncol = position[1] + dy[i];
                 console.log([nrow, ncol, i]);
-                document.getElementById('r' + nrow + 'c' + ncol).style.backgroundColor = "red";
+                var cell = document.getElementById('r' + nrow + 'c' + ncol);
+                cell.style.backgroundColor = "red";
+                cell.classList.add("plane"+this.identifier);
+
             }
             this.setProprieties(parseInt(GameBoard.Dimensions.cellHeight) * (position[0] - dx[0]), parseInt(GameBoard.Dimensions.cellWidth) * (position[1] - dy[0]));
             console.log(this.plane.style.top);
             document.getElementById('game-board').appendChild(this.plane);
+            return true;
         }
+        return false;
+
     },
 
-    clearAirplane : function(event)
+    clearAirplane : function()
     {
-        var position = this.getPositionFromId(event.target.id);
-        var dx = Plane[Plane.orientation].DX;
-        var dy = Plane[Plane.orientation].DY;
+        var position = this.position;
+        var dx = Plane[this.orientation].DX;
+        var dy = Plane[this.orientation].DY;
 
         for (var i = 0; i < dx.length; ++i) {
             var nrow = position[0] + dx[i];
@@ -117,14 +119,15 @@ Plane.prototype = {
             console.log([nrow, ncol, i]);
             document.getElementById('r' + nrow + 'c' + ncol).style.backgroundColor = 'white';
         }
-        document.getElementById('pull-right').removeChild(this.plane);
+        document.getElementById('game-board').removeChild(this.plane);
     },
 
-    rotateAirplane : function(event)
+    rotateAirplane : function()//event)
     {
-        clearAirplane(event);
-        Plane.changeOrientation(this.getNextOrientation(Plane.orientation));
-        placeAirplane(event);
+        this.clearAirplane();
+        do {
+        this.orientation = this.getNextOrientation(this.orientation);
+        }while(!this.placeAirplane());//event);
     },  
 
     allowDrop : function(event)
@@ -157,6 +160,8 @@ GameBoard = function (nrows, ncols, player)
     this._nrows = nrows;
     this._ncols = ncols;
     this._player = player;
+    this.placedPlanes = 0;
+    this.planes = {}
 
     for (var i = 0; i < nrows; ++i)
     {
@@ -169,7 +174,7 @@ GameBoard = function (nrows, ncols, player)
             
             //TODO another event listener !!! (need drag and drop)
             cell.addEventListener("click", this.placeAirplane.bind(this), false);
-            cell.addEventListener("right-click", this.rotateAirplane.bind(this), false);
+            cell.addEventListener("contextmenu", this.rotateAirplane.bind(this), false);
             row.appendChild(cell);  
         }       
     }   
@@ -179,7 +184,8 @@ GameBoard.Dimensions = {
     cellWidth : "40px",
     cellHeight : "40px"
 }
-
+GameBoard.MaxPlanes = 2
+GameBoard.nextPlaneID = 0
 GameBoard.prototype = {
     
     getPositionFromId : function(id)
@@ -187,10 +193,10 @@ GameBoard.prototype = {
         return [ parseInt(id.split(RegExp('r|c'))[1]), parseInt(id.split(RegExp('r|c'))[2]) ];
     },
     
-    validatePosition : function(position)
+    validatePosition : function(orientation, position)
     {
-        var dx = Plane[Plane.orientation].DX;
-        var dy = Plane[Plane.orientation].DY;
+        var dx = Plane[orientation].DX;
+        var dy = Plane[orientation].DY;
         for (var i = 0; i < dx.length; ++i) {
             var x = position[0] + dx[i];
             var y = position[1] + dy[i];
@@ -210,20 +216,37 @@ GameBoard.prototype = {
 
     placeAirplane : function(event)
     {
-        var plane = new Plane(Plane.Orientation.VerticalUp, this);
-        plane.placeAirplane(event);
+        if (this.placedPlanes == GameBoard.MaxPlanes)
+            return false;
+        ++this.placedPlanes;
+        var plane = new Plane(Plane.Orientation.VerticalUp, this, this.getPositionFromId(event.target.id));
+        this.planes["plane"+plane.identifier] = plane;
+        plane.placeAirplane();
 
-        //var planeContainer = new PlaneContainer(plane);
-        //document.getElementById("main-content").appendChild(planeContainer);
-
-    },
+   },
 
     rotateAirplane : function(event)
     {
+
         var position = this.getPositionFromId(event.target.id);
         if (document.getElementById(event.target.id).style.backgroundColor === "red")
         {
-
+            var planeID = null;
+            for (var i = 0; i < event.target.classList.length; ++i)
+                if (event.target.classList[i].match("plane[0-9]*")){
+                    planeID = event.target.classList[i];
+                    break;
+                }
+            if (event.stopPropagation)
+                event.stopPropagation();
+            event.cancelBubble = true;
+            if (!planeID)
+                return false;
+            var plane = this.planes[planeID];
+            if (!plane)
+                return false;
+            plane.rotateAirplane();
+            return false;
         }
 
     }
